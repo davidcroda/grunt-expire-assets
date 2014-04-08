57f240f688b66cf01901cf1a823d56eeb6be90ac
David Roda <davidcroda@gmail.com>
2014-04-08 11:02:21 -0400

    Remove debugging message

--- tasks/expire_assets.js
+++ tasks/expire_assets.js
@@ -28,7 +28,6 @@ module.exports = function (grunt) {
     // Iterate over all specified file groups.
     this.files.forEach(
       function (f) {
-        console.log(f.src);
         var src = f.src
           .map(utils.parseBase)
           .filter(utils.checkFile)
